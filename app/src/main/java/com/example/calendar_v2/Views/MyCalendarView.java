b7f78bc04f0043ee1c68fdd04092cd756c1369b8
haribo <g1004jay@naver.com>
2020-02-17 22:01:40 +0900

    Revert "Revert "ha""
    This reverts commit 4f96848c4016c498caad6011c6e016befb8c7ef8.

---
+++ app/src/main/java/com/example/calendar_v2/Views/MyCalendarView.java
@@ -0,0 +1,28 @@
+package com.example.calendar_v2.Views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+
+import androidx.annotation.Nullable;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.example.calendar_v2.MyConfig;
+import com.example.calendar_v2.R;
+
+public class MyCalendarView extends ViewPager {
+
+
+    public MyCalendarView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+
+
+
+
+
+}
