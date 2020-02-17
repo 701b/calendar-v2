db9afb53da050f8a87ec30d2311fea03478c53f5
haribo <g1004jay@naver.com>
2020-02-17 22:03:46 +0900

    Revert "Revert "Revert "ha"""
    This reverts commit b7f78bc04f0043ee1c68fdd04092cd756c1369b8.

--- app/src/main/java/com/example/calendar_v2/Activities/ConfigurationActivity.java
+++
@@ -1,6 +0,0 @@
-package com.example.calendar_v2.Activities;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-public class ConfigurationActivity extends AppCompatActivity {
-}
--- app/src/main/java/com/example/calendar_v2/MyConfig.java
+++
@@ -1,6 +0,0 @@
-package com.example.calendar_v2;
-
-public class MyConfig {
-    public final static int NUMBER_OF_ROWS = 6;
-    public final static int NUMBER_OF_COLUMNS = 7;
-}
